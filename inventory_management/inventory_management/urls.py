13eff915012e671987857c65df02d2c0509e10d6
arm00pv <zixen15@gmail.com>
2025-08-24 03:26:57 -0400

    Update urls.py

--- inventory_management/inventory_management/urls.py
+++ inventory_management/inventory_management/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView  # Import this
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("scanner.urls")),
+    path('admin/', admin.site.urls),
+    path('scanner/', include('scanner.urls')),
+    # Add this line to redirect the root URL to the scanner app
+    path('', RedirectView.as_view(url='/scanner/', permanent=True)),
 ]
