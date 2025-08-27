d863774268bd0cd98f526eed8dd68baf3677dc2a
arm00pv <zixen15@gmail.com>
2025-08-27 18:20:57 -0400

    Update urls.py

--- inventory_management/scanner/urls.py
+++ inventory_management/scanner/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.part_list, name='part_list)',
+    path('', views.part_list, name='part_list'),
     path('add/', views.add_part, name='add_part'),     
     path('', views.product_list, name='product_list'),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
