c87a20951a2c68c560c374c0b07540bb47eb3ce0
arm00pv <zixen15@gmail.com>
2025-08-27 18:11:38 -0400

    Update urls.py

--- inventory_management/inventory_management/urls.py
+++ inventory_management/inventory_management/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("scanner.urls")),
+    path("scanner/", include("scanner.urls")),
 ]
