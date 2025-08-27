82e341542aa6873f53d5a6e768be9e40924fdc31
arm00pv <zixen15@gmail.com>
2025-08-27 18:27:38 -0400

    Update urls.py

--- inventory_management/inventory_management/urls.py
+++ inventory_management/inventory_management/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("scanner/", include("scanner.urls")),
+    path('scanner/', include('scanner.urls')),  # Add this line
+    path('', RedirectView.as_view(url='/scanner/', permanent=True)),
 ]
